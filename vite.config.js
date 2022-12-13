383802f2c78d8d2c96018fca1a17f4d260c21761
AnatoliiGoIt <119327888+troloko1001@users.noreply.github.com>
2022-12-13 11:31:22 +0200

    add slashes to base in vite.confiq

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'DevGear-login-app',
+  base: '/DevGear-login-app/',
   plugins: [react()]
 })
